c6ecb09051f79c3dddfa73aff786453c8d670119
Brandon Mukum <brandonmukum@Brandons-MBP.hsd1.md.comcast.net>
2022-05-23 10:52:05 -0400

    ran terraform fmt and added comments

--- main.tf
+++ main.tf
@@ -1,19 +1,22 @@
+//ecr repo
 resource "aws_ecr_repository" "rearc" {
-  name = "rearc-app" 
+  name = "rearc-app"
 }
 
+//ecr cluster
 resource "aws_ecs_cluster" "rearc" {
-  name = "rearc-cluster" # Naming the cluster
+  name = "rearc-cluster"
 }
 
+// app task definition
 resource "aws_ecs_task_definition" "rearc_task" {
-  family                   = "rearc-task" # 
+  family                   = "rearc-task"
   container_definitions    = <<DEFINITION
   [
     {
       "name": "rearc-task",
       "image": "${aws_ecr_repository.rearc.repository_url}",
-      "essential": false,
+      "essential": true,
       "portMappings": [
         {
           "containerPort": 3000,
@@ -40,13 +43,14 @@ resource "aws_ecs_task_definition" "rearc_task" {
     }
   ]
   DEFINITION
-  requires_compatibilities = ["FARGATE"] 
-  network_mode             = "awsvpc"    
-  memory                   = 512         
-  cpu                      = 256         
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
   execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
 }
 
+//execution role for the task
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "ecsTaskExecutionRole"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
@@ -68,9 +72,10 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+//service with a security group tasks behind a load balancer
 resource "aws_ecs_service" "rearc" {
-  name            = "rearc-service"                            
-  cluster         = aws_ecs_cluster.rearc.id             
+  name            = "rearc-service"
+  cluster         = aws_ecs_cluster.rearc.id
   task_definition = aws_ecs_task_definition.rearc_task.arn
   launch_type     = "FARGATE"
   desired_count   = 2
@@ -78,19 +83,37 @@ resource "aws_ecs_service" "rearc" {
   load_balancer {
     target_group_arn = aws_lb_target_group.target_group.arn
     container_name   = aws_ecs_task_definition.rearc_task.family
-    container_port   = 3000 
+    container_port   = 3000
   }
 
   network_configuration {
     subnets          = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id, aws_default_subnet.default_subnet_c.id]
     assign_public_ip = true
+    security_groups  = [aws_security_group.service_security_group.id]
+  }
+}
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    // Only allowing traffic in from the load balancer security group
+    security_groups = [aws_security_group.load_balancer_security_group.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_default_vpc" "default_vpc" {
 }
 
-# Providing a reference to our default subnets
+//providing a reference to our default subnets
 resource "aws_default_subnet" "default_subnet_a" {
   availability_zone = "us-east-1a"
 }
@@ -104,33 +127,34 @@ resource "aws_default_subnet" "default_subnet_c" {
 }
 
 resource "aws_alb" "application_load_balancer" {
-  name               = "rearc-lb" 
+  name               = "rearc-lb"
   load_balancer_type = "application"
   subnets = [ # Referencing the default subnets
     aws_default_subnet.default_subnet_a.id,
     aws_default_subnet.default_subnet_b.id,
     aws_default_subnet.default_subnet_c.id
   ]
-  # Referencing the security group
+  //attach the sg to lb
   security_groups = [aws_security_group.load_balancer_security_group.id]
 }
 
 resource "aws_security_group" "load_balancer_security_group" {
   ingress {
-    from_port   = 80 
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0 
-    to_port     = 0 
-    protocol    = "-1" 
-    cidr_blocks = ["0.0.0.0/0"] 
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+// target group for lb
 resource "aws_lb_target_group" "target_group" {
   name        = "rearc-target-group"
   port        = 80
@@ -139,10 +163,11 @@ resource "aws_lb_target_group" "target_group" {
   vpc_id      = aws_default_vpc.default_vpc.id
   health_check {
     matcher = "200,301,302"
-    path = "/"
+    path    = "/"
   }
 }
 
+//lister for lb
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_alb.application_load_balancer.arn
   port              = "80"
@@ -153,6 +178,7 @@ resource "aws_lb_listener" "listener" {
   }
 }
 
+//log group to store logs from ecs tasks
 resource "aws_cloudwatch_log_group" "rearc-logs" {
   name = "rearc-logs"
 }
