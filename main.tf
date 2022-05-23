593f3e6202aec0d5b6826fc8d4f75a3858ae0911
Brandon Mukum <brandonmukum@Brandons-MBP.hsd1.md.comcast.net>
2022-05-23 08:45:00 -0400

    included log groups for ecs task

--- main.tf
+++ main.tf
@@ -1,173 +1,158 @@
-resource "aws_vpc" "rearc_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+resource "aws_ecr_repository" "rearc" {
+  name = "rearc-app" 
 }
 
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.rearc_vpc.id
+resource "aws_ecs_cluster" "rearc" {
+  name = "rearc-cluster" # Naming the cluster
+}
+
+resource "aws_ecs_task_definition" "rearc_task" {
+  family                   = "rearc-task" # 
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "rearc-task",
+      "image": "${aws_ecr_repository.rearc.repository_url}",
+      "essential": false,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "logConfiguration": {
+          "logDriver": "awslogs",
+          "options": {
+            "awslogs-group": "rearc-logs",
+            "awslogs-region": "us-east-1",
+            "awslogs-stream-prefix": "ecs"
+          }
+      },
+      "environment": [
+        {
+          "name": "SECRET_WORD",
+          "value": "BrandonRearc"
+        }
+      ],
+      
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] 
+  network_mode             = "awsvpc"    
+  memory                   = 512         
+  cpu                      = 256         
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "rearc" {
+  name            = "rearc-service"                            
+  cluster         = aws_ecs_cluster.rearc.id             
+  task_definition = aws_ecs_task_definition.rearc_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 2
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = aws_ecs_task_definition.rearc_task.family
+    container_port   = 3000 
+  }
+
+  network_configuration {
+    subnets          = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id, aws_default_subnet.default_subnet_c.id]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
   availability_zone = "us-east-1a"
-  cidr_block        = "10.0.1.0/32"
-
-  tags = {
-    "Name" = "rearc-public-subnet-1"
-  }
 }
 
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.rearc_vpc.id
+resource "aws_default_subnet" "default_subnet_b" {
   availability_zone = "us-east-1b"
-  cidr_block        = "10.0.2.0/32"
-
-  tags = {
-    "Name" = "rearc-public-subnet-2"
-  }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.rearc_vpc.id
-
-  tags = {
-    "Name" = "Internet-Gateway"
-  }
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "us-east-1c"
 }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.rearc_vpc.id
-
-  tags = {
-    "Name" = "Public-RouteTable"
-  }
-}
-
-resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table_association" "public_rt_association_1" {
-  route_table_id = aws_route_table.public_rt.id
-  subnet_id      = aws_subnet.public_subnet_1.id
-}
-
-resource "aws_route_table_association" "public_rt_association_2" {
-  route_table_id = aws_route_table.public_rt.id
-  subnet_id      = aws_subnet.public_subnet_2.id
-}
-
-resource "aws_security_group" "sg" {
-
-  name        = "CustomSG"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.rearc_vpc.id
-  egress = [
-    {
-      description      = "for all outgoing traffics"
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-
-  ingress = [
-    {
-      description = "Allow inbound traffic"
-      from_port   = 3000
-      to_port     = 3000
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    },
-    {
-      description = "Allow inbound ssh"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-  tags = {
-    Name = "AWS security group block"
-  }
-
-}
-
-resource "aws_instance" "instance" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
-  subnet_id       = aws_subnet.public_subnet_1.id
-  security_groups = [aws_security_group.sg.id, ]
-  key_name        = "rearc"
-  user_data       = "bootstrap.sh"
-
-  tags = {
-    "Name" = "rearc-instance"
-  }
-
-}
-data "aws_availability_zones" "azs" {}
-
-resource "aws_lb_target_group" "tg" {
-  name        = "TargetGroup"
-  port        = 3000
-  target_type = "instance"
-  protocol    = "tcp"
-  vpc_id      = aws_vpc.rearc_vpc.id
-}
-
-resource "aws_alb_target_group_attachment" "tgattachment" {
-  target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = aws_instance.instance.id
-}
-
-resource "aws_lb" "lb" {
-  name               = "ALB"
-  internal           = false
+resource "aws_alb" "application_load_balancer" {
+  name               = "rearc-lb" 
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.sg.id, ]
-  subnets            = flatten([aws_subnet.public_subnet_1.*.id, aws_subnet.public_subnet_2.*.id])
+  subnets = [ # Referencing the default subnets
+    aws_default_subnet.default_subnet_a.id,
+    aws_default_subnet.default_subnet_b.id,
+    aws_default_subnet.default_subnet_c.id
+  ]
+  # Referencing the security group
+  security_groups = [aws_security_group.load_balancer_security_group.id]
 }
 
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = "3000"
-  protocol          = "tcp"
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80 
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
 
+  egress {
+    from_port   = 0 
+    to_port     = 0 
+    protocol    = "-1" 
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "rearc-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_default_vpc.default_vpc.id
+  health_check {
+    matcher = "200,301,302"
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
+    target_group_arn = aws_lb_target_group.target_group.arn
   }
-
-#   default_action {
-#     type = "redirect"
-
-#     redirect {
-#       port        = "443"
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
 }
-# resource "aws_lb_listener_rule" "static" {
-#   listener_arn = aws_lb_listener.front_end.arn
-#   priority     = 100
 
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.tg.arn
-
-#   }
-# }
+resource "aws_cloudwatch_log_group" "rearc-logs" {
+  name = "rearc-logs"
+}
